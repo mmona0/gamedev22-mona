6c0d6da041967baa834b03b66d1855317502bb20
Mona <113378489+mmona0@users.noreply.github.com>
2024-12-05 13:53:54 +0100

    Update auswertung.php

--- public/auswertung.php
+++ public/auswertung.php
@@ -1,1 +1,27 @@
+<!DOCTYPE html>
+<html>
 
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Auswertung</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+  <h1>Auswertung</h1>
+  <?php
+    echo $_POST['standort'];
+    echo $_POST['fellzustand'];
+    echo $_POST['haltung'];
+    echo $_POST['bcs'];
+    echo $_POST['training'];
+
+?>
+
+
+
+  <p><a href="" onclick="window.history.back();">Zurück zur Startseite</a>
+</body>
+
+</html>
